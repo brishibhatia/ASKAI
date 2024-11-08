a1608342dd8a17ff93c12d00a7f057f294201ad6
YRishi Bhatia <brishi11128@gmail.com>
2024-11-08 12:39:04 +0530

    added css stying to the sidebar

--- AskAIProject/src/components/sidebar/sidebar.jsx
+++ AskAIProject/src/components/sidebar/sidebar.jsx
@@ -6,11 +6,12 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <img src={assets.menu_icon} alt="" />
+        <img className="menu" src={assets.menu_icon} alt="" />
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
           <p>New Chat</p>
         </div>
+
         <div className="recent">
           <p className="recent-title">Recent</p>
           <div className="recent-entry">
@@ -19,6 +20,7 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
+
       <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.question_icon} alt="" />
